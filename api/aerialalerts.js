3c29d2c005c9c6104e16c0945e7d03feed909c7b
lowflicker <middleflicker@icloud.com>
2025-02-25 13:08:22 +0200

    Update aerialalerts.js

--- api/aerialalerts.js
+++ api/aerialalerts.js
@@ -1,15 +1,3 @@
-export default async function handler(req, res) {
-  try {
-    const response = await fetch('https://ubilling.net.ua/aerialalerts/?xml=true');
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.text();
-    res.setHeader('Content-Type', 'application/xml');
-    res.status(200).send(data);
-  } catch (error) {
-    console.error('Fetch error:', error);
-    res.status(500).json({ error: 'Error fetching data' });
-  }
+export default function handler(req, res) {
+  res.status(200).json({ message: "API is working!" });
 }
