40ef952b1a30e36f965281e9a8e3ae2ab9a2f10d
lowflicker <middleflicker@icloud.com>
2025-02-25 12:50:33 +0200

    Create aerialalerts.js

---
+++ api/aerialalerts.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch('https://ubilling.net.ua/aerialalerts/?xml=true');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.text();
+    res.setHeader('Content-Type', 'application/xml');
+    res.status(200).send(data);
+  } catch (error) {
+    console.error('Fetch error:', error);
+    res.status(500).json({ error: 'Error fetching data' });
+  }
+}
